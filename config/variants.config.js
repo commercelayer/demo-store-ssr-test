74b2fdb30dacd1a4c2bdbd6789834ef91d3baa75
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-01-19 21:48:13 +0100

    chore: update configuration

---
+++ config/variants.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/** @type { import('@commercelayer/demo-store-types').VariantConfig[] } */
+const variantsConfig = [
+  {
+    field: 'flavor',
+    appearance: 'tag'
+  },
+  {
+    field: 'size',
+    appearance: 'tag'
+  },
+]
+
+module.exports = variantsConfig
